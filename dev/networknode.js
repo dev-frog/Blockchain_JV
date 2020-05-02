cdd301c83cae5e6d5f088b249c6e81d7baf68768
dev-frog <kali.nisga@gmail.com>
2020-05-03 05:24:41 +0600

    Node register and broadcast add

---
+++ dev/networknode.js
@@ -0,0 +1,97 @@
+const express = require('express')
+const app = express()
+const bodyparser = require('body-parser')
+const  Blockchain = require('./blockchain');
+const bitconin = new Blockchain();
+const uuid = require('uuid');
+const port = process.argv[2];
+const rp = require('request-promise');
+
+
+const nodeAddress = uuid.v1().split('-').join('');
+
+
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({extended:false}));
+
+app.get('/',function(req,res){
+    res.send("<h1>404 Pages not found </h1>");
+})
+
+app.get('/blockchain',function(req,res){
+    res.send(bitconin);
+});
+
+app.post('/transaction',function(req,res){
+    const BlockIndex = bitconin.createNewTransaction(req.body.amount,req.body.sender,req.body.recipient);
+    res.send(`Transcation will be added in block ${BlockIndex}`);
+})
+
+
+app.get('/mine',function(req,res){
+    const lastblock = bitconin.getLastBlock();
+    const previousBlockHash = lastblock['hash'];
+    const currentblockData = {
+        transaction : bitconin.pendingTransactions,
+        index : lastblock['index'] + 1
+    }
+    const nonce = bitconin.proofOfWork(previousBlockHash,currentblockData);
+    const blockhash = bitconin.hashBlock(previousBlockHash,currentblockData,nonce);
+    bitconin.createNewTransaction(12.5,"00",nodeAddress);
+    const newblock = bitconin.createNewBlock(nonce,previousBlockHash,blockhash);
+    res.json({
+        note: "New Block mind Successfully",
+        block: newblock
+    })
+});
+
+app.post('/register-and-broadcast-node',function(req,res){
+    const newNodeURL = req.body.newNodeURL;
+    if(bitconin.networkNodes.indexOf(newNodeURL) == -1) bitconin.networkNodes.push(newNodeURL);
+    const regNodePromises = [];
+    bitconin.networkNodes.forEach(networkNodeURL => {
+        const requestOptions ={
+            uri: networkNodeURL + '/register-node',
+            method: 'POST',
+            body: { newNodeURL: newNodeURL},
+            json: true
+        };
+        regNodePromises.push(rp(requestOptions));
+    });
+    Promise.all(regNodePromises).then(data =>{
+        const bulkRegisterOptions = {
+            uri: newNodeURL + '/register-nodes-bulk',
+            method:'POST',
+            body: {allNetworkNodes: [...bitconin.networkNodes,bitconin.currentNodeURL]},
+            json:true
+        };
+        return rp(bulkRegisterOptions);
+    }).then(data =>{
+        res.json({node : 'New node registe sucessfully'})
+    });
+});
+
+app.post('/register-node',function(req,res){
+    const newNodeUrl = req.body.newNodeURL;
+    const nodeNotAllreadyPresent = bitconin.networkNodes.indexOf(newNodeUrl) == -1;
+    const notCurrentNode = bitconin.currentNodeURL !== newNodeUrl;
+    if(nodeNotAllreadyPresent && notCurrentNode) bitconin.networkNodes.push(newNodeUrl);
+    res.json({note : 'New Node registerd successfully'});
+
+});
+
+app.post('/register-nodes-bulk',function(req,res){
+    const allNetworkNodes = req.body.allNetworkNodes;
+    allNetworkNodes.forEach(networkNodeURL =>{
+        const nodeNotAlreadyPresent = bitconin.networkNodes.indexOf(networkNodeURL) == -1;
+        const NotCureentNode = bitconin.currentNodeURL !== networkNodeURL;
+        if(nodeNotAlreadyPresent && NotCureentNode) bitconin.networkNodes.push(networkNodeURL);
+    });
+
+    res.json({note:'Bulk registration successful.'});
+
+});
+
+app.listen(port,function(){
+    console.log(`Lising on port ${port}`)
+})
